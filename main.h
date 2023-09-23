1438e516d9f85f7fc69379bcdeb6afcf7b56904d
imanenadif <nadifimane43@gmail.com>
2023-09-22 18:00:58 -0700

    add typedef struct

--- main.h
+++ main.h
@@ -28,3 +28,14 @@ struct fmt
 };
 
 
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
